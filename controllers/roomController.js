36cc7f6bac8f1d8b1b99249d29cbcf2581a91217
deepak <deepakkumardash313@gmail.com>
2022-01-08 19:16:11 +0530

    update room

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -2,6 +2,22 @@
 
 import Room from '../models/room';
 
+// CREATE A NEW ROOM
+export const newRoom = async (req, res) => {
+  try {
+    const room = await Room.create(req.body);
+    res.status(200).json({
+      success: true,
+      room,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
 // GET ALL ROOMS
 export const getAllRooms = async (req, res) => {
   try {
@@ -42,10 +58,22 @@ export const getSingleRoom = async (req, res) => {
   }
 };
 
-// CREATE A NEW ROOM
-export const newRoom = async (req, res) => {
+// UPDATE ROOM
+export const updateRoom = async (req, res) => {
   try {
-    const room = await Room.create(req.body);
+    let room = await Room.findById(req.query.id);
+    if (!room) {
+      return res.status(404).json({
+        success: false,
+        error: 'Room with that id does not exist',
+      });
+    }
+
+    room = await Room.findByIdAndUpdate(req.query.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
     res.status(200).json({
       success: true,
       room,
--- pages/api/rooms/[id].js
+++ pages/api/rooms/[id].js
@@ -2,11 +2,12 @@
 
 import nextConnect from 'next-connect';
 import dbConnect from '../../../config/dbConnect';
-import { getSingleRoom } from '../../../controllers/roomController';
+import { getSingleRoom, updateRoom } from '../../../controllers/roomController';
 dbConnect();
 
 const handler = nextConnect();
 
 handler.get(getSingleRoom);
+handler.put(updateRoom);
 
 export default handler;
