7fc58b1de8d1435993a0b79ec0ee8a9772fb6408
deepak <deepakkumardash313@gmail.com>
2022-01-25 20:16:37 +0530

    add : login component & page

---
+++ pages/login.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import Login from '../components/auth/Login';
+import Layout from '../components/Layout/Layout';
+
+const LoginPage = () => {
+  return (
+    <Layout title="Login">
+      <Login />
+    </Layout>
+  );
+};
+
+export default LoginPage;
