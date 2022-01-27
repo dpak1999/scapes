7521f189242b54273969422ae625d2467ef9d4cc
deepak <deepakkumardash313@gmail.com>
2022-01-28 00:12:11 +0530

    handle webhook for stripe

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -6,6 +6,7 @@ import User from '../models/user';
 import Booking from '../models/bookings';
 import APIFeatures from '../utils/apiFeatures';
 import absoluteUrl from 'next-absolute-url';
+import getRawBody from 'raw-body';
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
@@ -35,4 +36,48 @@ export const stripeCheckoutSession = catchAsyncErrors(async (req, res) => {
   });
 
   res.status(200).json(session);
+});
+
+// CREATE A NEW BOOKING AFTER PAYMENT
+export const webhookCheckout = catchAsyncErrors(async (req, res) => {
+  const rawBody = await getRawBody(req);
+
+  try {
+    const signature = req.headers['stripe-signature'];
+    const event = stripe.webhooks.constructEvent(
+      rawBody,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+
+    if (event.type === 'checkout.session.completed') {
+      const session = event.data.object;
+
+      const room = session.client_reference_id;
+      const user = await User.findOne({ email: session.customer_email });
+      const amountPaid = session.amount_total / 100;
+      const paymentInfo = {
+        id: session.payment_intent,
+        status: session.payment_status,
+      };
+      const checkInDate = session.metadata.checkInDate;
+      const checkOutDate = session.metadata.checkOutDate;
+      const daysOfStay = session.metadata.daysOfStay;
+
+      await Booking.create({
+        room,
+        user,
+        checkInDate,
+        checkOutDate,
+        daysOfStay,
+        amountPaid,
+        paymentInfo,
+        paidAt: Date.now(),
+      });
+
+      res.status(200).json({ success: true });
+    }
+  } catch (error) {
+    console.log('Error in stripe checkout payment => ', error);
+  }
 });
